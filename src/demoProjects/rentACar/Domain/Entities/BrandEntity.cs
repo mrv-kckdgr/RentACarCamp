0d5a4dfc025ad585b6e6ef725a5f2c3418fba309
Kucukdogru <m42erve@hotmail.com>
2022-08-21 14:47:52 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Domain/Entities/BrandEntity.cs
@@ -0,0 +1,24 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class BrandEntity : Entity
+    {
+        public string Name { get; set; }
+
+        public BrandEntity()
+        {
+        }
+
+        public BrandEntity(int id, string name) : this()
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/Persistence/Repositories/BrandEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using Application.Services.Repositories;
+using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class BrandEntityRepository : EfRepositoryBase<BrandEntity, BaseDbContext>, IBrandEntityRepository
+    {
+        public BrandEntityRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
