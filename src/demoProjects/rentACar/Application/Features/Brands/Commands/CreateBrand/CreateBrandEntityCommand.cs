8fbbdb72e0f8036c5e99776f383d6eb3a6cc40dd
Kucukdogru <m42erve@hotmail.com>
2022-08-22 23:51:47 +0300

    August 22, Add Business Rules & Global Exception Handling

---
+++ src/demoProjects/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandEntityCommand.cs
@@ -0,0 +1,44 @@
+﻿using Application.Features.Brands.Dtos;
+using Application.Features.Brands.Rules;
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandEntityCommand : IRequest<CreatedBrandEntityDto>
+    {
+        public string Name { get; set; }
+
+        public class CreateBrandEntityCommandHandler : IRequestHandler<CreateBrandEntityCommand, CreatedBrandEntityDto>
+        {
+            private readonly IBrandEntityRepository _brandRepository;
+            private readonly IMapper _mapper;
+            private readonly BrandEntityBusinessRules _brandEntityBusinessRules;
+
+            public CreateBrandEntityCommandHandler(IBrandEntityRepository brandRepository, IMapper mapper, BrandEntityBusinessRules brandEntityBusinessRules)
+            {
+                _brandRepository = brandRepository;
+                _mapper = mapper;
+                _brandEntityBusinessRules = brandEntityBusinessRules;
+            }
+
+            public async Task<CreatedBrandEntityDto> Handle(CreateBrandEntityCommand request, CancellationToken cancellationToken)
+            {
+                await _brandEntityBusinessRules.BrandNameCanNotBeDuplicatedWhenInserted(request.Name);
+                
+                BrandEntity mappedBrandEntity = _mapper.Map<BrandEntity>(request);
+                BrandEntity createdBrandEntity = await _brandRepository.AddAsync(mappedBrandEntity);
+                CreatedBrandEntityDto createdBrandEntityDto = _mapper.Map<CreatedBrandEntityDto>(createdBrandEntity);
+
+                return createdBrandEntityDto;
+            }
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/Application/Features/Brands/Rules/BrandEntityBusinessRules.cs
@@ -0,0 +1,27 @@
+﻿using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Rules
+{
+    public class BrandEntityBusinessRules
+    {
+        private readonly IBrandEntityRepository _brandEntityRepository;
+
+        public BrandEntityBusinessRules(IBrandEntityRepository brandEntityRepository)
+        {
+            _brandEntityRepository = brandEntityRepository;
+        }
+        public async Task BrandNameCanNotBeDuplicatedWhenInserted(string name)
+        {
+            IPaginate<BrandEntity> result = await _brandEntityRepository.GetListAsync(b => b.Name == name);
+            if (result.Items.Any()) throw new BusinessException("BrandEntity name exists.");
+        }
+    }
+}
